aa442af1e782c0e12fa31e471b07141b010c1b9a
YohaanS <yohaan.singla@gmail.com>
2024-01-16 22:04:10 -0700

    Added Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <h1 className="text-red-600">Hello World</h1>
+    <Button>Click Me</Button>
   )
 }
