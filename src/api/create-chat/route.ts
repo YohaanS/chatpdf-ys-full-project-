057b0ab3211184e76cecfdeebdb06821b0fe750b
YohaanS <yohaan.singla@gmail.com>
2024-02-04 00:16:41 -0700

    Done for the day

--- src/api/create-chat/route.ts
+++ src/api/create-chat/route.ts
@@ -3,10 +3,14 @@ import { NextResponse } from "next/server"
 // /api/create-chat
 export async function POST(req: Request, res: Response) {
     try {
-        const body = await req.json()
-        const {file_key, file_name} = body
+        const body = await req.json();
+        const {  file_key, file_name } = body
+        console.log(file_key, file_name)
     } catch (error) {
         console.error(error)
-        return NextResponse.json({error: "internal server error"}, { status: 500 })
+        return NextResponse.json(
+            {error: "internal server error"}, 
+            { status: 500 }
+        );
     }
 }
